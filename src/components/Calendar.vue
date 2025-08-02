84a898472b07e6a8b8d708403480abd43896997b
Nugie Jaya Nugraha <90244101+jayanugie@users.noreply.github.com>
2025-08-02 23:39:07 +0700

    feat: calendar

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,45 +1,24 @@
 <template>
-  <div class="bg-dark-gray p-6 rounded-lg shadow-lg w-1/3 text-white">
-    <h2 class="text-xl font-semibold mb-4">October 2020</h2>
-    <div class="grid grid-cols-7 text-center text-gray-400 mb-2">
-      <span>Mo</span><span>Tu</span><span>We</span><span>Th</span><span>Fr</span
-      ><span>Sa</span><span>Su</span>
-    </div>
-    <div class="grid grid-cols-7 gap-1 text-center">
-      <span class="p-1"></span>
-      <span class="p-1"></span>
-      <span class="p-1"></span>
-      <span class="p-1">1</span>
-      <span class="p-1">2</span>
-      <span class="p-1">3</span>
-      <span class="p-1">4</span>
-      <span class="p-1">5</span>
-      <span class="p-1">6</span>
-      <span class="p-1">7</span>
-      <span class="p-1">8</span>
-      <span class="p-1">9</span>
-      <span class="p-1">10</span>
-      <span class="p-1">11</span>
-      <span class="p-1">12</span>
-      <span class="p-1">13</span>
-      <span class="p-1 bg-green-500 rounded-full font-bold text-white">14</span>
-      <span class="p-1">15</span>
-      <span class="p-1">16</span>
-      <span class="p-1">17</span>
-      <span class="p-1">18</span>
-      <span class="p-1">19</span>
-      <span class="p-1">20</span>
-      <span class="p-1">21</span>
-      <span class="p-1">22</span>
-      <span class="p-1">23</span>
-      <span class="p-1">24</span>
-      <span class="p-1">25</span>
-      <span class="p-1">26</span>
-      <span class="p-1">27</span>
-      <span class="p-1">28</span>
-      <span class="p-1">29</span>
-      <span class="p-1">30</span>
-      <span class="p-1">31</span>
-    </div>
+  <div class="w-1/3">
+    <Calendar
+      borderless
+      class="shadow-lg rounded-lg"
+      is-dark="false"
+      :attributes="attrs"
+      expanded
+    />
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import { Calendar } from "v-calendar";
+import "v-calendar/style.css";
+
+const attrs = ref([
+  {
+    highlight: true,
+    dates: new Date(),
+  },
+]);
+</script>
